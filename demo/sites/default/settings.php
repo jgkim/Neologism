f7109323f308aa163c3d7d54c3eee6b52a6abad6
scorlosquet <scorlosquet@9bc17e61-e048-0410-9b95-ddf33fbbb69f>
2008-05-31 22:46:49 +0000

    oops. this time: restore the settings.php to original drupal version, necessary for automatic installation

--- demo/sites/default/settings.php
+++ demo/sites/default/settings.php
@@ -90,9 +90,7 @@
  *   $db_url = 'mysqli://username:password@localhost/databasename';
  *   $db_url = 'pgsql://username:password@localhost/databasename';
  */
-
-$db_url = 'mysql://root:root@localhost/dneotest';
-
+$db_url = 'mysql://username:password@localhost/databasename';
 $db_prefix = '';
 
 /**
