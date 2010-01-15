e15bbf9aea45e78d731eaf3d042dc1849d2f7e94
guidocecilio <guidocecilio@9bc17e61-e048-0410-9b95-ddf33fbbb69f>
2010-01-15 02:19:45 +0000

    =Added behaviour to hide/show the inverse selection widget depending on whether the range is shown

--- neologism/js/neologism.js
+++ neologism/js/neologism.js
@@ -27,11 +27,15 @@ if( Drupal.jsEnabled ) {
 	if ( $('#edit-resource-type-1').attr('checked') ) {
     	$('#range-group-datatypes').hide();
     	$('#range-group-fieldrange2').show();
+    	// the inverse selection widget should be shown if the range field also are shown
+    	$('#inverse-treeview').show();
     }
 	// A literal (string, number, date, ...)
     else if ( $('#edit-resource-type-2').attr('checked') ) {
     	$('#range-group-fieldrange2').hide();
     	$('#range-group-datatypes').show();
+    	// the inverse selection widget should be hidden if the range field also are hidden
+    	$('#inverse-treeview').hide();
     }
 	// Either
     else if ( $('#edit-resource-type-3').attr('checked') ) {
